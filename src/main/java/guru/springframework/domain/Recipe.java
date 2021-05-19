68607810929993f3efb4354c6a8c2c077c2bbb89
calenarrna <calenarrna@gmail.com>
2021-05-19 13:35:53 +0200

    Refactored Ingredients and Recipe creation in Bootstrap

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -118,6 +118,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
@@ -126,6 +127,12 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Recipe addIngredient (Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        ingredients.add(ingredient);
+        return this;
     }
 
     public Difficulty getDifficulty() {
