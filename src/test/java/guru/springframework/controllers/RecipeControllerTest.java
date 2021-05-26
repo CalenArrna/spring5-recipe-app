39c3c12c0eaed4bd5f7442a88e4732c2d832cf2e
calenarrna <calenarrna@gmail.com>
2021-05-26 19:58:44 +0200

    Implemented: show.html, Can display stored recipes by ID. Added attribute check to RecipeControllerTest. Corrected missing attributes in RecipeBootstrap.

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -9,9 +9,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class RecipeControllerTest {
 
@@ -33,9 +34,11 @@ public class RecipeControllerTest {
 
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
 
+        when(recipeService.getRecipeById(anyLong())).thenReturn(recipe);
+
         mockMvc.perform(get("/recipe/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
-
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
 }
