e068940b8e8f1a00b8db0c99a597cfa64824afc7
calenarrna <calenarrna@gmail.com>
2021-05-31 12:17:07 +0200

    Implemented Create Recipe. Still have missing data. Added CircleCi config.yml

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,15 +1,17 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
-@RequestMapping("recipe")
 @Controller
 public class RecipeController {
 
@@ -19,10 +21,22 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping({"/show/{recipe_id}", "/{recipe_id}"})
+    @RequestMapping({"recipe/show/{recipe_id}", "recipe/{recipe_id}"})
     public String getRecipeView(@PathVariable Long recipe_id, Model model) {
         Recipe recipe = recipeService.getRecipeById(recipe_id);
         model.addAttribute("recipe", recipe);
         return "recipe/show";
+    }
+
+    @RequestMapping({"recipe/new"})
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate (@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
