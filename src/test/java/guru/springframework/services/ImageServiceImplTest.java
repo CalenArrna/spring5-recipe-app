c9dae84ee833bc39dad7e9b6ead1b991334677a2
calenarrna <calenarrna@gmail.com>
2021-06-07 19:46:06 +0200

    Implemented ImageController, and tests, created base of image saving feature.

---
+++ src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ImageServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveImageFile() {
+    }
+}
